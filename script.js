d90186e3c8334059c3561897d23c1d548765941f
devrmnd <dhevasenraa@gmail.com>
2025-09-30 14:02:12 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function openInstagram() {
+  alert("Menuju Instagram...");
+  window.open("https://instagram.com/username_anda", "_blank");
+}
+
+function openWhatsApp() {
+  alert("Membuka WhatsApp...");
+  window.open("https://wa.me/6281234567890", "_blank");
+}
+
+function openX() {
+  alert("Menuju profil X (Twitter)...");
+  window.open("https://x.com/username_anda", "_blank");
+}
